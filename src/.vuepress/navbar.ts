fccf8d0f2c060169bca6d19e49672b3804bc4588
huyu <yhu8836@gmail.com>
2023-10-13 14:27:01 +0800

    修改 logo + 首页布局

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,7 +2,6 @@ import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
     "/",
-    "/demo/",
     {
         text: "指南",
         icon: "lightbulb",
@@ -14,9 +13,5 @@ export default navbar([
             }
         ],
     },
-    {
-        text: "V2 文档",
-        icon: "book",
-        link: "https://theme-hope.vuejs.press/zh/",
-    },
+    "/demo/"
 ]);
